5d4bc27b1984a875d6614eb35fa8578025ad93d4
wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-11 20:55:17 +0100

    added

--- src/Component/Superhero.jsx
+++ src/Component/Superhero.jsx
@@ -6,6 +6,7 @@ export const Superhero = () => {
         <div className=' font-bold text-center'>
              <h1 className=' mt-16 text-5xl'>New year, new plans.</h1>
              <p>Your workspace to write, organize, and collaborate. With AI by your side.</p>
+             <p>organize</p>
         </div>
     </>
   )
