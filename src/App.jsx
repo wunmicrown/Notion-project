2956af464ffa9d8130a366c68c7d35f28f8a8ca5
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-12 21:35:41 +0100

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SecondNavbar from './Component/Secondnavbar'
 import { Superhero } from './Component/Superhero'
 import Superheroimg from './Component/Superheroimg'
 import Cardsection from './Component/Cardsection'
+import Carousel from './Component/Carousel'
 
 const App = () => {
   const [isStrue, setisStrue] = useState(false)
@@ -14,6 +15,7 @@ const App = () => {
       <Superhero/>
       <Superheroimg/>
       <Cardsection/>
+      <Carousel/>
     </>
   )
 }
