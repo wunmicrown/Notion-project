05568358e7153d47fa141f6fbabeba1c7d6ef61c
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-12 16:20:51 +0100

    commited

--- src/Component/Superheroimg.jsx
+++ src/Component/Superheroimg.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import img from '../assets/image-folder/hero_x.png'
 const Superheroimg = () => {
   return (
-    <div>Superheroimg</div>
+    <>
+        <img className=' w-[650px] mx-auto mt-8' src={img} alt="heroimg" />
+    </>
   )
 }
 
