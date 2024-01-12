eb8d66d077ed6292758622e9b9ebea6178d2b0bf
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-12 15:52:46 +0100

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Navbar from './Component/Navbar'
 import SecondNavbar from './Component/Secondnavbar'
 import { Superhero } from './Component/Superhero'
+import Superheroimg from './Component/Superheroimg'
 
 const App = () => {
   const [isStrue, setisStrue] = useState(false)
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar setisStrue={setisStrue} isStrue={isStrue} />
       <SecondNavbar isStrue={isStrue}/>
       <Superhero/>
+      <Superheroimg/>
     </>
   )
 }
---
+++ src/Component/Superheroimg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Superheroimg = () => {
+  return (
+    <div>Superheroimg</div>
+  )
+}
+
+export default Superheroimg
