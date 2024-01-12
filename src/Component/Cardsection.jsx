61abec59fb750154da772f3eb274dad9079c37fc
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-12 21:25:01 +0100

    commited

--- src/Component/Cardsection.jsx
+++ src/Component/Cardsection.jsx
@@ -14,12 +14,12 @@ const Cardsection = () => {
                 <div className="text col-span-4 gap-4 flex flex-row overflow-x-auto">
                     {data.map((item, index) => (
                         <div className={` hover:}`}>
-                            <div key={index} class="block cursor-pointer max-w-sm p-6 border rounded-lg shadow bg-gray-800 border-gray-700 hover:bg-gray-700">
-                                <div className="text flex gap-3 text-white text-2xl items-center first-letter:uppercase">
+                            <div key={index} class="block cursor-pointer max-w-sm p-6 border rounded-lg shadow bg-slate-100 border-x-white hover:bg-white ">
+                                <div className="text flex gap-3 text-black text-2xl items-center first-letter:uppercase">
                                     <p>{item.header}</p>
                                     {item.icon}
                                 </div>
-                                <h5 class="mb-2 text-sm font-medium tracking-tight text-gray-900 dark:text-white">
+                                <h5 class="mb-2 text-sm font-medium tracking-tight text-gray-900 dark:text-black">
                                     {item.content}
                                 </h5>
                                 <div class="text-red-700 hidden hover:block transition-all duration-300 ease-in-out" onMouseOver={()=>  setcontent(index)}>
@@ -41,14 +41,14 @@ const data = [
         id: 1,
         icon: <FaRegNewspaper />,
         header: "AI",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.",
+        content: "Ask literally anything. Notion we answer.",
         leranMore: "learn more",
     },
     {
         id: 2,
         icon: <FaRegNewspaper />,
         header: "Wikis",
-        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.",
+        content: "Centeralize your knowledge. No more hunting for answers.",
         leranMore: "learn more",
     },
     {
