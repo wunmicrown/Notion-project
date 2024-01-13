a8148780bd927058309133842d8fa6bacc8e225c
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-13 20:35:53 +0100

    commited

--- src/Component/Carousel.jsx
+++ src/Component/Carousel.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import img from '../assets/image-folder/Kombi_Hero.png';
 import img1 from '../assets/image-folder/engineering.png';
 import img2 from '../assets/image-folder/projects.png';
-import img2 from '../assets/image-folder/meeting-notes.png';
+import img3 from '../assets/image-folder/meeting-notes.png';
 
 const Carousel = () => {
   const images = [img, img1, img2, img3]; 
@@ -18,11 +18,11 @@ const Carousel = () => {
 
   return (
     <>
-      <div id="default-carousel" className="relative w-full" data-carousel="slide">
+      <div id="default-carousel" className="relative w-10/12 mx-auto" data-carousel="slide">
         <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
           {images.map((image, index) => (
             <div key={index} className={`duration-700 ease-in-out ${index === currentSlide ? '' : 'hidden'}`} data-carousel-item>
-              <img src={image} alt={`Slide ${index + 1}`} className="absolute block w-full h-full object-cover" />
+              <img src={image} alt={`Slide ${index + 1}`} className="absolute block w-full  object-cover flex mx-auto" />
             </div>
           ))}
         </div>
@@ -38,16 +38,16 @@ const Carousel = () => {
             ></button>
           ))}
         </div>
-        <button type="button" onClick={handlePrevSlide} className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <button type="button" onClick={handlePrevSlide} className="absolute top-0 start-0 z-30 flex items-center justify-center  px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white dark:bg-white-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
             </svg>
             <span class="sr-only">Previous</span>
         </span>
         </button>
-        <button type="button" onClick={handleNextSlide} className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <button type="button" onClick={handleNextSlide} className="absolute top-0 end-0 z-30 flex items-center justify-center  px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full  bg-white dark:bg-white-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
             </svg>
